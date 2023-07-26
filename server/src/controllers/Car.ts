a7984b0c02651752135161193b73d4a7d9bf9fe8
Rama Chandar Rao Shambu <rshambu6620@conestogac.on.ca>
2023-07-26 10:38:26 -0400

    adding car images for car register

--- server/src/controllers/Car.ts
+++ server/src/controllers/Car.ts
@@ -120,6 +120,11 @@ class Car implements IController {
   register = async (req: Request, resp: Response): Promise<void> => {
     const car: CarType = req.body;
 
+    const buffer = readFileSync(
+      path.join(__dirname, "../asset/img/bottle.png")
+    );
+    car.images = [buffer, buffer];
+
     const error: { [key: string]: string } =
       this.handleUserInputValidation(car);
     if (Object.keys(error).length > 0) {
